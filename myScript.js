4eb6f2a9e909d3402205b1b782dd4548c832b1ae
Donovan Mosley <dmosley28@gmail.com>
2020-05-29 07:09:50 -0400

    Adds random dot generator to JavaScript, .lights image, styles .lights image

--- myScript.js
+++ myScript.js
@@ -25,3 +25,18 @@ function openPage(pageName,elmnt,color) {
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
   
+
+var html = '';
+var red;
+var green;
+var blue;
+var rgbColor;
+
+for ( var i = 1; i <= 5; i +=1) {
+red = Math.floor(Math.random() * 256 );
+green = Math.floor(Math.random() * 256 );
+blue = Math.floor(Math.random() * 256 );
+rgbColor = 'rgb(' + red + ',' + green + ',' + blue + ')';
+html += '<div style="background-color:' + rgbColor + '"></div>';
+}
+document.write(html);
