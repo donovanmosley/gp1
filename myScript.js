80b8ce703479ffee630c997fc5a8d536aeab6146
Donovan Mosley <dmosley28@gmail.com>
2020-06-18 07:16:24 -0400

    Adds 4 home page buttons, styles Subscribe image and lights

--- myScript.js
+++ myScript.js
@@ -32,7 +32,7 @@ var green;
 var blue;
 var rgbColor;
 
-for ( var i = 1; i <= 5; i +=1) {
+for ( var i = 1; i <= 8; i +=1) {
 red = Math.floor(Math.random() * 256 );
 green = Math.floor(Math.random() * 256 );
 blue = Math.floor(Math.random() * 256 );
