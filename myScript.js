3f67a44d813be0d8cf141717fa81a72a3b6a64ab
Donovan Mosley <dmosley28@gmail.com>
2020-07-22 07:01:30 -0400

    Added asbyssinian image, added module JavaScript function for Abyssinian Guinea Pig

--- myScript.js
+++ myScript.js
@@ -25,6 +25,34 @@ function openPage(pageName,elmnt,color) {
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
   
+//This script creates a modal box:
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 
 var html = '';
 var red;
